1563eb93fd5c343f834fb34f12e643af410355cf
esofronova <essofronova@gmail.com>
2021-12-13 18:41:01 -0800

    style changes

--- frontend/src/Components/Welcome.jsx
+++ frontend/src/Components/Welcome.jsx
@@ -3,7 +3,7 @@ export default function Welcome(props) {
   return (
     <div>
       <h1>{`Welcome, @${props.user}`}!!!</h1>
-      <form action="/logout" method="post">
+      <form action="/logout" method="post" className="m-0">
         <button>Sign Out</button>
       </form>
     </div>
