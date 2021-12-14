4c13b2f9bc0db39007c992e8eb2bf44b608d173f
esofronova <essofronova@gmail.com>
2021-12-13 19:59:00 -0800

    final changes

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,12 @@
 const express = require('express');
 const cookieParser = require('cookie-parser')
-const users = require('./database');
 
 const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cookieParser());
 
-app.post('/process_login', (req, res) => {
-  let { username, password } = req.body;
-  users.some(item => item.username === username && item.password === password)
-    ?
-    (res.cookie('username', username), res.redirect('/'))
-    :
-    res.redirect('/?msg=fail');
-});
-
-app.post('/logout', (req, res) => {
-  res.clearCookie('username');
-  res.redirect('/');
-});
-
+const users = [];
 app.get('/api', (req, res) => res.send(users));
 
 app.post('/check-username', (req, res) => {
@@ -34,6 +20,20 @@ app.post('/check-username', (req, res) => {
 app.post('/add-user', (req, res) => {
   users.push(req.body.data);
   res.cookie('username', req.body.data.username);
+  res.redirect('/');
+});
+
+app.post('/process_login', (req, res) => {
+  let { username, password } = req.body;
+  users.some(item => item.username === username && item.password === password)
+    ?
+    (res.cookie('username', username), res.redirect('/'))
+    :
+    res.redirect('/?msg=fail');
+});
+
+app.post('/logout', (req, res) => {
+  res.clearCookie('username');
   res.redirect('/');
 });
 
